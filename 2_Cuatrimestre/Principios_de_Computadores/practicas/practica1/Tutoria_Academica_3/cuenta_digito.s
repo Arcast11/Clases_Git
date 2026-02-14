d360a0190c896a74ffb50334403424b328a3d68f
Armando Jose Castro Peña <armando.castro.31@ull.edu.es>
2026-02-14 16:43:20 +0000

    cuenta_digito_resuelto

--- 2_Cuatrimestre/Principios_de_Computadores/practicas/practica1/Tutoria_Academica_3/cuenta_digito.s
+++ 2_Cuatrimestre/Principios_de_Computadores/practicas/practica1/Tutoria_Academica_3/cuenta_digito.s
@@ -3,8 +3,7 @@
 #Realizar un programa que cuente el número de veces que aparece un dígito en
 #un número entero positivo.
 
-#Por ejemplo si el usuario introduce como numero el valor 12242 y como dígito el valor 2,
-#debe imprimir por la pantalla el número 3 (que es las veces que aparece 2 en 12242).
+#Por ejemplo si el usuario introduce como numero el valor 12242 y como dígito el valor 2
 
 #Los pasos a seguir deben ser:
 
@@ -49,11 +48,11 @@
 #}
 
 	.data
-strPrograma:	.asciiz  "\nPrograma que cuenta el numero de veces que aparece un digito en numero entero positivo.\n"
-strElNumero:	.asciiz  "\nIntroduzca un numero entero positivo (negativo para finalizar el programa): "
-strInroDigito:	.asciiz  "Introduzca un digito (0-9): "
-strElNumero:	.asciiz  "El numero de veces que aparece el digito en el numero es: "
-strFin:			.asciiz  "\nFin del programa.\n"
+strPrograma:		.asciiz  "\nPrograma que cuenta el numero de veces que aparece un digito en numero entero positivo.\n"
+strIntroNumero:		.asciiz  "\nIntroduzca un numero entero positivo (negativo para finalizar el programa): "
+strInroDigito:		.asciiz  "Introduzca un digito (0-9): "
+strElNumero:		.asciiz  "El numero de veces que aparece el digito en el numero es: "
+strFin:				.asciiz  "\nFin del programa.\n"
 
 ############################################
 # Tabla de asignación de registros
@@ -70,33 +69,33 @@ strFin:			.asciiz  "\nFin del programa.\n"
 main:
 #  std::cout << "\nPrograma que cuenta el numero de veces que aparece un digito en numero entero positivo.\n";
 
-	li	$v0,4
-	la	$a0, strPrograma
+	li		$v0,4
+	la		$a0, strPrograma
 	syscall
 
 #  while (true) {
 
-whileTrue:
+while_true:
 	# No hace falta condicion porque siempre se cumple → siempre entra
 
 
 #    int numero;
 #    std::cout << "\nIntroduzca un numero entero positivo (negativo para finalizar el programa): ";
 
-	li	$v0,4
-	la	$a0, strElNumero
+	li		$v0,4
+	la		$a0, strIntroNumero
 	syscall
 
 #    std::cin >> numero;
 
-li	$v0,5
+	li		$v0,5
 	syscall
 	move	$s4,$v0
 
 #    if (numero < 0)
 #      break;
 
-	bltz	$s4,whileFin
+	bltz	$s4,while_fin
 
 #    int digito;
 #    do {
@@ -105,37 +104,81 @@ do_while_digito:
 
 #      std::cout << "Introduzca un digito (0-9): ";
 
-	li	$v0,4
-	la	$a0,strInroDigito
+	li		$v0,4
+	la		$a0,strInroDigito
 	syscall
 
 #      std::cin >> digito;
 
-	li 	$v0,5
+	li 		$v0,5
 	syscall
 	move 	$s5,$v0
 
 #    } while ((digito < 0) || (digito > 9));
+	bltz 	$s5, do_while_digito
+	li 		$t0, 9
+	bgt 	$s5, $t0, do_while_digito
 
-while_digitolt0_digitogt9
 
 #    int cuenta = 0;
+
+	li 		$s6, 0
+
+do_while_contar:
+
 #    do {
 #      int resto = numero % 10;
-#      numero /= 10;
-#      if ( resto == digito )
-#        cuenta++;
-#    } while ( numero != 0 );
-#    std::cout << "El numero de veces que aparece el digito en el numero es: "
-#        << cuenta << '\n';
-#  }
-	b	whileTrue
 
-whileFin:
+	li 		$t1, 10
+
+#      numero /= 10;
+
+	div 	$s4, $t1
+	mfhi	$t7
+	mflo	$s4
+
+#      if ( resto == digito )
+
+if_resto_eq_digito:
+
+	bne		$t7, $s5, while_num_neqz
+
+#        cuenta++;
+
+	addi 	$s6, $s6, 1
+
+#    } while ( numero != 0 );
+
+while_num_neqz:
+
+	bnez	$s4, do_while_contar
+
+#    std::cout << "El numero de veces que aparece el digito en el numero es: "
+
+	li 		$v0, 4
+	la		$a0, strElNumero
+	syscall
+
+#        << cuenta << '\n';
+
+	li		$v0, 1
+	move 	$a0, $s6
+	syscall
+
+#  }
+
+	b	while_true
+
+while_fin:
 #  std::cout << "\nFin del programa.\n";
+
+    li $v0, 4
+    la $a0, strFin
+    syscall
+
 #}
 
-	li	$v0,10
+	li		$v0,10
 	syscall
 
 
