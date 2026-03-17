fc1ecb215b98ec51a6cac950e78ad0b3c29f9d9b
Arcast11 <armando.castro.31@ull.edu.es>
2026-03-17 21:24:38 +0000

    fase 3 practica ayed

--- 2_Cuatrimestre/Algoritmos/Practicas/practica_3/sparse_vector_t.h
+++ 2_Cuatrimestre/Algoritmos/Practicas/practica_3/sparse_vector_t.h
@@ -1,21 +1,22 @@
-// AUTOR: 
-// FECHA: 
-// EMAIL: 
+// AUTOR:
+// FECHA:
+// EMAIL:
 // VERSION: 4.0
 // ASIGNATURA: Algoritmos y Estructuras de Datos
 // PRÁCTICA Nº: 3
 // ESTILO: Google C++ Style Guide
 // COMENTARIOS:
-// 
+//
 
 #ifndef SPARSE_VECTORT_H_
 #define SPARSE_VECTORT_H_
 
-#include <iostream>
 #include <math.h>  // fabs
 
-#include "vector_t.h"
+#include <iostream>
+
 #include "pair_t.h"
+#include "vector_t.h"
 
 #define EPS 1.0e-6
 
@@ -27,7 +28,7 @@ class sparse_vector_t {
   // constructores
   sparse_vector_t(const int = 0);
   sparse_vector_t(const vector_t<double>&,
-		  const double = EPS); // constructor normal
+                  const double = EPS);      // constructor normal
   sparse_vector_t(const sparse_vector_t&);  // constructor de copia
 
   // operador de asignación
@@ -35,7 +36,7 @@ class sparse_vector_t {
 
   // destructor
   ~sparse_vector_t();
-  
+
   // getters
   int get_nz(void) const;
   int get_n(void) const;
@@ -43,7 +44,7 @@ class sparse_vector_t {
   // getters-setters
   pair_double_t& at(const int);
   pair_double_t& operator[](const int);
-  
+
   // getters constantes
   const pair_double_t& at(const int) const;
   const pair_double_t& operator[](const int) const;
@@ -59,7 +60,6 @@ class sparse_vector_t {
   // bool IsNotZero(const double, const double = EPS) const;
 };
 
-
 bool IsNotZero(const double val, const double eps = EPS) {
   return fabs(val) > eps;
 }
@@ -69,7 +69,27 @@ sparse_vector_t::sparse_vector_t(const int n) : pv_(n), nz_(0), n_(n) {}
 // FASE II
 sparse_vector_t::sparse_vector_t(const vector_t<double>& v, const double eps)
     : pv_(), nz_(0), n_(0) {
-  // poner el código aquí
+  // Primer bucle: contamos el número de valores no nulos
+
+  for (size_t i{0}; i < v.get_size(); i++) {
+    if (IsNotZero(v[i])) {
+      nz_++;
+    }
+  }
+  // Redimencionamos el vector pv_ (vector par: valor + indice)
+  pv_.resize(nz_);
+
+  // Le damos valor 0 a nz para realizar el segundo bucle
+  nz_ = 0;
+
+  // Segundo bucle: Asigna el valor al vector de pares
+  for (size_t i{0}; i < v.get_size(); i++) {
+    if (IsNotZero(v[i])) {
+      pv_[nz_].set(v[i], i);
+      nz_++;
+    }
+    n_++;
+  }
 }
 
 // constructor de copia
@@ -88,22 +108,16 @@ sparse_vector_t& sparse_vector_t::operator=(const sparse_vector_t& w) {
 
 sparse_vector_t::~sparse_vector_t() {}
 
-inline int sparse_vector_t::get_nz() const {
-  return nz_;
-}
+inline int sparse_vector_t::get_nz() const { return nz_; }
 
-inline int sparse_vector_t::get_n() const {
-  return n_;
-}
+inline int sparse_vector_t::get_n() const { return n_; }
 
 pair_double_t& sparse_vector_t::at(const int i) {
   assert(i >= 0 && i < get_nz());
   return pv_[i];
 }
 
-pair_double_t& sparse_vector_t::operator[](const int i) {
-  return at(i);
-}
+pair_double_t& sparse_vector_t::operator[](const int i) { return at(i); }
 
 const pair_double_t& sparse_vector_t::at(const int i) const {
   assert(i >= 0 && i < get_nz());
@@ -115,11 +129,10 @@ const pair_double_t& sparse_vector_t::operator[](const int i) const {
 }
 
 // E/S
-void sparse_vector_t::write(std::ostream& os) const { 
+void sparse_vector_t::write(std::ostream& os) const {
   os << get_n() << "(" << get_nz() << "): [ ";
-  for (int i = 0; i < get_nz(); i++)
-    os << pv_[i] << " ";
-	os << "]" << std::endl;
+  for (int i = 0; i < get_nz(); i++) os << pv_[i] << " ";
+  os << "]" << std::endl;
 }
 
 std::ostream& operator<<(std::ostream& os, const sparse_vector_t& sv) {
